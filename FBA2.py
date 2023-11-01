3d60d370874d72647d658c30e49add7cd2a1bbbb
sᴇʀᴅᴀʀ öᴢᴛüʀᴋ <116160977+Serdar-Trk@users.noreply.github.com>
2023-11-01 13:35:39 +0300

    Update FBA2.py

--- FBA2.py
+++ FBA2.py
@@ -1,23 +1,20 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import os  # for secure password handling
+import os
 
-pd.set_option('display.max_rows', 2000)
-pd.set_option('display.max_columns', 2000)
-pd.set_option('display.width', 2000)
-pd.set_option('display.max_rows', None)
 sales_by_date2022 = pd.read_excel('sales_by_date2022.xlsx')
 sales_by_date2023 = pd.read_excel('sales_by_date2023.xlsx')
 
 def main():
     st.set_page_config(layout="wide")
 
-
-
     fig = px.bar(sales_by_date2022, x='Fatura_Tarihi', y='Counts', color='Satış_Adedi',
                  labels={'Fatura_Tarihi': 'Tarih', 'Counts': 'Sayı'},
                  title='Tarihe Göre Benzersiz Satış Adedi Dağılımı(2022)')
+
+    # Grafik yüksekliğini ayarla
+    fig.update_layout(height=700)
 
     # Grafiği gösterin
     st.plotly_chart(fig, use_container_width=True)
@@ -26,9 +23,12 @@ def main():
                  labels={'Fatura_Tarihi': 'Tarih', 'Counts': 'Sayı'},
                  title='Tarihe Göre Benzersiz Satış Adedi Dağılımı(2023)')
 
+    # Grafik yüksekliğini ayarla
+    fig.update_layout(height=700)
+
     # Grafiği gösterin
     st.plotly_chart(fig, use_container_width=True)
 
-
 if __name__ == "__main__":
     main()
+
