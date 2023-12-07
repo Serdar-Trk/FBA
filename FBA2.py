7d7b84e67b1d530529ae8976861c1f1527da6fa4
sᴇʀᴅᴀʀ öᴢᴛüʀᴋ <116160977+Serdar-Trk@users.noreply.github.com>
2023-12-07 12:46:19 +0300

    Update FBA2.py

--- FBA2.py
+++ FBA2.py
@@ -1,34 +1,65 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import os
+import os  # for secure password handling
+import plotly.graph_objects as go
 
-sales_by_date2022 = pd.read_excel('sales_by_date2022.xlsx')
-sales_by_date2023 = pd.read_excel('sales_by_date2023.xlsx')
+pd.set_option('display.max_rows', 2000)
+pd.set_option('display.max_columns', 2000)
+pd.set_option('display.width', 2000)
+pd.set_option('display.max_rows', None)
+
+from glob import glob
+
+import plotly.graph_objs as go
+
+
+monthly_data = pd.read_excel('monthly_data.xlsx')
+
 
 def main():
     st.set_page_config(layout="wide")
+    password = st.text_input("Şifreyi girin", type="password")
+    correct_password = os.environ.get('PASSWORD', "1514")  # getting password from environment variable
+    if password != correct_password:
+        return
 
-    fig = px.bar(sales_by_date2022, x='Fatura_Tarihi', y='Counts', color='Satış_Adedi',
-                 labels={'Fatura_Tarihi': 'Tarih', 'Counts': 'Sayı'},
-                 title='Tarihe Göre Benzersiz Satış Adedi Dağılımı(2022)')
+    fig = go.Figure()
 
-    # Grafik yüksekliğini ayarla
-    fig.update_layout(height=700)
+    fig.add_trace(go.Scatter(x=monthly_data['Ay'],
+                             y=monthly_data['Ortalama_Satış_Adedi'],
+                             mode='lines+markers',
+                             line=dict(color='orangered', width=2),
+                             marker=dict(color='orangered', size=8),
+                             name='Ortalama_Satış_Adedi'))
 
-    # Grafiği gösterin
+    # Grafiği düzenle
+    fig.update_layout(title='Aylık Ortalama Satış Adedi',
+                      xaxis_title='Tarih',
+                      yaxis_title='Oran',
+                      template='plotly_dark',
+                      height=600,
+                      font=dict(size=20, color='black'),
+                      legend=dict(x=0.01, y=0.99))
+
+    # Yüzdelik değerleri grafiğe ekleyelim
+    for i, row in monthly_data.iterrows():
+        fig.add_annotation(
+            x=row['Ay'],
+            y=row['Ortalama_Satış_Adedi'],
+            text=f"{row['Ortalama_Satış_Adedi']:.2}",
+            showarrow=True,
+            arrowhead=1,
+            arrowsize=3,
+            arrowwidth=1,
+            arrowcolor='black',
+            ax=0,
+            ay=-20
+        )
+
     st.plotly_chart(fig, use_container_width=True)
 
-    fig = px.bar(sales_by_date2023, x='Fatura_Tarihi', y='Counts', color='Satış_Adedi',
-                 labels={'Fatura_Tarihi': 'Tarih', 'Counts': 'Sayı'},
-                 title='Tarihe Göre Benzersiz Satış Adedi Dağılımı(2023)')
-
-    # Grafik yüksekliğini ayarla
-    fig.update_layout(height=700)
-
-    # Grafiği gösterin
-    st.plotly_chart(fig, use_container_width=True)
 
 if __name__ == "__main__":
     main()
-
